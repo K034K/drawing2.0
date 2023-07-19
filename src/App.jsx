dddd5e95abd0732f099ee4e35827144088ef8998
Vladyslav Mrynskyi <mrynskyiw@gmail.com>
2023-07-19 16:36:21 +0300

    changing branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useRef } from "react";
 
 //create grid based;based on width and height of the screen
 // grid contains squares that will be filled with colors
@@ -20,41 +21,22 @@ import React, { useState } from "react";
 const activeColor = "blue";
 
 const createGrid = (width, height) => {
-    
     const grid = [];
-    for (let i = 0; i < height; i++) {
-        for (let j = 0; j < width; j++) {
-            grid.push(createSquare(width, height));
-        }
+    for (let i = 0; i < height + width; i++) {
+        grid.push(createSquare(width, height, i));
     }
-    return (
-        <div className="grid">
-            {grid.map((square) => (
-                <div
-                    className="square"
-                    style={{
-                        backgroundColor: square.color,
-                        width: square.widthSquare,
-                        height: square.heightSquare,
-                    }}
-                    onClick={changeColor()}
-                ></div>
-            ))}
-        </div>
-    );
+    return <div className="grid">{grid.map((square) => square)}</div>;
 };
 
 //color picker
 
 const createColorPicker = () => {
-    
     const colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "black", "white"];
-    
-    return
-        
+
+    return;
+
     <div className="colorPicker">
         {colors.map((color) => (
-            
             <div
                 className="color"
                 style={{
@@ -63,8 +45,7 @@ const createColorPicker = () => {
                 onClick={setActiveColor(color)}
             ></div>
         ))}
-    </div>
-        ;
+    </div>;
 };
 
 //set active color
@@ -75,46 +56,30 @@ const setActiveColor = (color) => {
 
 //create a square
 
-const createSquare = (width, height) => {
-    return {
-        color: "darkgrey",
-        widthSquare: window.innerWidth / width,
-        heightSquare: window.innerHeight / height,
-    };
+const createSquare = (width, height, id) => {
+    const dS = (
+        <div
+            key={id.toString()}
+            ref={id.toString()}
+            id={id.toString()}
+            className="square"
+            onClick={e=>changeColor(e, id)}
+            style={{
+                backgroundColor: "darkCyan",
+                width: window.innerWidth / width,
+                height: window.innerHeight / height,
+            }}
+        ></div>
+    );
+    
+    return dS;
 };
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            color: "darkblue",
-        };
-    }
-    render() {
-        return (
-            <div
-                className="square"
-                style={{
-                    backgroundColor: this.state.color,
-                }}
-            ></div>
-        );
-    }
+//change color of square
 
-    changeColor() {
-        this.setState({
-            color: activeColor,
-        });
-
-    }
-
-}
-
-
-const changeColor = () => {
-    return (square) => {
-        square.color = activeColor;
-    };
+const changeColor = (e, id) => {
+    const square = document.getElementById(id.toString());
+    square.style.backgroundColor = activeColor;
 };
 
 export default function App() {
