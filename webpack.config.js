c4b921d6902cb4178e3583e587db34621150bf80
Vlad <k03ak@pop-os.localdomain>
2023-07-21 16:00:40 +0300

    adding express... work in progress...

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$|jsx$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              ["@babel/preset-env", { targets: "defaults" }],
-              "@babel/preset-react",
-            ],
-            plugins: ["@babel/plugin-proposal-class-properties"],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
-    ],
-  },
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public"),
+        publicPath: "/",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$|jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+            },
+        ],
+    },
+    devServer: {
+        hot: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ], resolve: {
+        extensions: ['.js', '.jsx'],
+    },
 };
